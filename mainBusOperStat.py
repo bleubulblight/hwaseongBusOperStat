654aa5babf26b8e469c2380edf32eaefdda52aa9
BleuBulbLight <byulorg2@gmail.com>
2022-10-26 13:18:01 +0900

    add main python file

---
+++ mainBusOperStat.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import re
+import pdb
+import time
+import datetime
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as mticker
+
+def main():
+    print('hello world!')
+
+if __name__ == '__main__':
+    main()
