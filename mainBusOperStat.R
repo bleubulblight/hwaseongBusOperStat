0a770ee4ac595ce732a8a8e470afc990d32120cf
hippotimbo <59413070+hippotimbo@users.noreply.github.com>
2022-11-03 19:20:57 +0900

    timbo2

--- mainBusOperStat.R
+++ mainBusOperStat.R
@@ -25,20 +25,23 @@ for (i in 1:length(folders)) {
 
 #### OPEN DATA ####
 for (i in 1:nrow(avail_index)) {
+  #### general import ####
   this_row <- avail_index[i,]
-  if (this_row$date != "2022-10-25") {next}
   this_data <- read.csv(paste0("./rawData/",this_row$folder,"/",this_row$filename))
   this_data$timestamp <- substr(this_data$querytime,10,15)
   this_data$hms <- lubridate::hms(format(strptime(this_data$timestamp, format = "%H%M%S"), format = "%H:%M:%S"))
   this_data["remainSeatCnt"][this_data["remainSeatCnt"] == -1] <- NA
-
+  
+  #### divide dispatches by cluster ####
   clustering <- Ckmeans.1d.dp::Ckmeans.1d.dp(as.numeric(filter(this_data,stationSeq==1)$timestamp),k=c(1:30))
   this_data[this_data$stationSeq==1,"dispatch"] <- clustering$cluster
-  if ("dep_schedule" %in% ls()) {
-  dep_schedule <- rbind(dep_schedule,cbind(line=this_row$line,aggregate(timestamp~plateNo+dispatch,FUN=min,this_data)))
-  } else {
-    dep_schedule <- cbind(line=this_row$line,aggregate(timestamp~plateNo+dispatch,FUN=min,this_data))
-  }
+  
+  #### save departure schedule ####
+  # if ("dep_schedule" %in% ls()) {
+  # dep_schedule <- rbind(dep_schedule,cbind(line=this_row$line,aggregate(timestamp~plateNo+dispatch,FUN=min,this_data)))
+  # } else {
+  #   dep_schedule <- cbind(line=this_row$line,aggregate(timestamp~plateNo+dispatch,FUN=min,this_data))
+  # }
   
   # for (bus in unique(this_data$plateNo)) {
   #  this_bus <- this_data |>
@@ -46,11 +49,16 @@ for (i in 1:nrow(avail_index)) {
   #  
   # }
   
-  # timespace <- ggplot(data=this_data,mapping=aes(x=timestamp,y=stationSeq,colour=plateNo)) +
-  #   labs(title=this_row$line, x = "시간", y = "정류장순번", color = "차량번호") +
-  #   theme(plot.title = element_text(hjust = 0.5)) +
-  #   scale_x_discrete(labels=NULL) +
-  #   geom_point(size=2)
+  line_length <- max(this_data$stationSeq)
+  
+  timespace <- ggplot(data=this_data,mapping=aes(x=timestamp,y=stationSeq,colour=plateNo)) +
+    geom_point(size=2) +
+    labs(title=this_row$line, x = "시간", y = "정류장순번", color = "차량번호") +
+    theme(plot.title = element_text(hjust = 0.5),panel.grid.major.y =element_line(colour="grey")) +
+    scale_x_discrete(labels=NULL) +
+    scale_y_continuous(limits=c(0,line_length))
+  
+  timespace
   #ggsave(paste0("./plots/timespace/",this_row$line,"_",this_row$date,".png"),timespace)  
 }
 
